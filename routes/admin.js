d9bb15b4202bbef2679f4c0b3edb7d4fe5128cee
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-10-16 16:28:10 +0530

    USER MANAGMENT

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var session = require("express-session");
 var Comeon = require("../Models/mongodb");
-var products = require("../Models/products");
+// var products = require("../Models/products");
 
 const auth = function (req, res, next) {
     if (req.session.loggedIn) {
